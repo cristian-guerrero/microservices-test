cd86ec66db847cfd06c38b976f2f75ffa648f9f9
Cristian Guerrero <guerrero.chavez.cristia@gmail.com>
2020-11-03 21:36:12 -0500

    post components base

--- comments/index.js
+++ comments/index.js
@@ -2,21 +2,22 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto')
-
+const cors = require('cors')
 const app = express()
 
 app.use(bodyParser.json())
+app.use(cors())
 
 const commentsByPostId = {}
 
 app.get('/posts/:id/comments', (req, res) => {
 
-  res.send(commentsByPostId[req.params.id] ||)
+  res.send(commentsByPostId[req.params.id] || [])
 
 })
 
 
-app.post('/posts/:/id/comments', (req, res) => {
+app.post('/posts/:id/comments', (req, res) => {
 
   const commentId = randomBytes(4).toString('hex')
   const { content } = req.body
@@ -27,7 +28,7 @@ app.post('/posts/:/id/comments', (req, res) => {
 
   commentsByPostId[req.params.id] = comments
 
-    req.status(201).send(comments)
+    res.status(201).send(comments)
 
 })
 

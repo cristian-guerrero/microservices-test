b8d2ecf8b0736867ba4180f70c1d1deca0c89197
Cristian Guerrero <guerrero.chavez.cristia@gmail.com>
2020-11-10 22:02:38 -0500

    added docker file to client

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 // const eventBusURL = 'http://localhost:4005/events'
-const eventBusURL = 'http://event-bus-clusterip-srv:4005/events'
+// const eventBusURL = 'http://event-bus-clusterip-srv:4005/events'
 
 ReactDOM.render(
     <App/>,

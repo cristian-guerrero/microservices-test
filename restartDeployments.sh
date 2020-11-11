0a4fe791ea86fa561fbb90a69d5231d66457ff44
Cristian Guerrero <guerrero.chavez.cristia@gmail.com>
2020-11-10 22:28:15 -0500

    added script to restart deployments

---
+++ restartDeployments.sh
@@ -0,0 +1,7 @@
+#deploys=`kubectl get deployments -n apps | tail -n1 | cut -d ' ' -f 1`
+deploys=`kubectl get deployments | tail -n6 | cut -d ' ' -f 1`
+for deploy in $deploys; do
+    echo 'kubectl rollout restart deployments/' $deploy
+  # kubectl rollout restart deployments/$deploy -n apps
+  kubectl rollout restart deployments/$deploy 
+done
